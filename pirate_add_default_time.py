a70bc5c79b4ce031f88f8d4f97823d9dd5fde4fc
ard0gg <arielyles@gmail.com>
2020-01-23 10:29:34 -0500

    added a default work time

--- pirate_add_default_time.py
+++ pirate_add_default_time.py
@@ -4,18 +4,22 @@ from datetime import time
 from tasklib import Task, local_zone
 
 DEFAULT_TIME = time(21,0,0)  # Your wanted default time
-
+DEFAULT_WORK_TIME = time(17,0,0) #Default Work Time
 def is_local_midnight(timestamp):
     return timestamp.astimezone(local_zone).time() == time(0,0,0)
 
-def set_default_time(timestamp):
+def set_default_time(timestamp, defTime):
     return timestamp.astimezone(local_zone).replace(
-        hour=DEFAULT_TIME.hour,
-        minute=DEFAULT_TIME.minute,
-        second=DEFAULT_TIME.second,
+        hour=defTime.hour,
+        minute=defTime.minute,
+        second=defTime.second,
         )
 
 def hook_default_time(task):
     if task['due'] and is_local_midnight(task['due']):
-        task['due'] = set_default_time(task['due'])
-        print("Default due time has been set.")
+	if '@work' in task['tags']:
+		task['due'] = set_default_time(task['due'], DEFAULT_WORK_TIME)
+		print("Default due time has been set to work default.")
+	else: 
+        	task['due'] = set_default_time(task['due'], DEFAULT_TIME)
+        	print("Default due time has been set.")
